4108f1eb4a566654ebc18c3aa64692927294ba4c
Leandro Scardua <leandroscar@gmail.com>
2020-08-23 13:47:42 -0400

    Update windows-node.ps1

--- windows-node.ps1
+++ windows-node.ps1
@@ -1,11 +1,51 @@
+Write-Host "Rename the computer to win19-n1"
+#Rename
+Rename-Computer -NewName "win19-n1" | Out-Null
 
-# Rename the interface
-Rename-NetAdapter -Name Get-NetAdapter -Name * -Physical -NewName "Ethernet"
+Write-Host "Rename the network interface to Ethernet"
+# Rename network interface to Ethernet
+Rename-NetAdapter -Name Get-NetAdapter -Name * -Physical -NewName "Ethernet" | Out-Null
 
+Write-Host "Enabling Winrm Access"
+# Enable winrm access 
+Set-WSManQuickConfig -Force
+
+Write-Host "Disabling Windows Updates"
+#Disable Windows Update
+Net stop wuauserv
+%systemroot%\system32\Cscript %systemroot%\system32\scregedit.wsf /AU 1
+Net start wuauserv
+Set-Service -Name "wuauserv" -StartupType Disabled -Status Stopped -Force | Out-Null
+
+Write-Host "Uninstalling Windows-Defender Feature"
 # Uninstall Windows Defender
-Uninstall-WindowsFeature Windows-Defender
+Uninstall-WindowsFeature Windows-Defender | Out-Null
+
+# Update the NuGet Module
+Write-Host "Updating The Nuget Package Management"
+Install-PackageProvider -Name NuGet -Force | Out-Null
+
+# Install the Windows Feature Containers on the Server
+Write-Host "Installing Windows Containers Feature"
+Install-WindowsFeature -Name Containers | Out-Null
+
+# Install the Docker Engine Enterprise on the Server
+Write-Host "Installing Docker Engine Enterprise"
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force | Out-Null
+
+# Install the client interface to manage the Docker Engine Enterprise
+Write-Host "Installing Interface de Gerenciamento of the Docker"
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force | Out-Null
+
+# Installing requirements on the Windows Node to join Kubernetes Cluster
 
 curl.exe -LO https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/PrepareNode.ps1
 .\PrepareNode.ps1 -KubernetesVersion v1.18.0
 
-#kubeadm join 
+pause
+
+Write-Host "Please, hit enter to reboot the server to complete the configuration"
+
+Restart-Computer
+
+
