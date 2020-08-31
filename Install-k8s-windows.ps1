45c60595af99c546c2a104cc5f7ef39fc105130a
Leandro Scardua <leandroscar@gmail.com>
2020-08-31 14:40:35 -0400

    Update Install-k8s-windows.ps1

--- Install-k8s-windows.ps1
+++ Install-k8s-windows.ps1
@@ -2,3 +2,25 @@
 
 curl.exe -LO https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/PrepareNode.ps1
 .\PrepareNode.ps1 -KubernetesVersion v1.18.0 | Out-Null
+
+# install fix after reboot
+
+Set-Content C:\k\StartKubelet.ps1 -Value {
+$FileContent = Get-Content -Path "/var/lib/kubelet/kubeadm-flags.env"
+$global:KubeletArgs = $FileContent.Trim("KUBELET_KUBEADM_ARGS=`"")
+
+$netId = docker network ls -f name=host --format "{{ .ID }}"
+if ($netId.Length -lt 1) {
+    docker network create -d nat host
+}
+
+$cmd = "C:\k\kubelet.exe $global:KubeletArgs --cert-dir=$env:SYSTEMDRIVE\var\lib\kubelet\pki --config=/var/lib/kubelet/config.yaml --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --hostname-override=$(hostname) --pod-infra-container-image=`"mcr.microsoft.com/k8s/core/pause:1.2.0`" --enable-debugging-handlers --cgroups-per-qos=false --enforce-node-allocatable=`"`" --network-plugin=cni --resolv-conf=`"`" --log-dir=/var/log/kubelet --logtostderr=false --image-pull-progress-deadline=20m"
+
+Invoke-Expression $cmd
+}
+
+
+
+
+
+
