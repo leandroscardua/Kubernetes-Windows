48867da1b4048051c4a9fc27df8b7d23520d9068
Leandro Scardua <leandroscar@gmail.com>
2021-12-14 11:03:03 -0500

    Update Install-k8s-windows.ps1

--- Install-k8s-windows.ps1
+++ Install-k8s-windows.ps1
@@ -1,27 +1,13 @@
-param(
-  [Parameter(Mandatory=$true)]
-  [String]$k8sversion
-)
+#param(
+#  [Parameter(Mandatory=$true)]
+#  [String]$k8sversion
+#)
 #Enable Donwloadas
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Installing requirements on the Windows Node to join Kubernetes Cluster
 
-curl.exe -LO https://github.com/kubernetes-sigs/sig-windows-tools/releases/download/v0.1.5/PrepareNode.ps1
-.\PrepareNode.ps1 -KubernetesVersion "$k8sversion" | Out-Null
+curl.exe -LO https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1
+.\PrepareNode.ps1 -KubernetesVersion v1.23.0 | Out-Null
 
-# install fix after reboot
-
-#Set-Content C:\k\StartKubelet.ps1 -Value {
-#$FileContent = Get-Content -Path "/var/lib/kubelet/kubeadm-flags.env"
-#$global:KubeletArgs = $FileContent.Trim("KUBELET_KUBEADM_ARGS=`"")
-
-#$netId = docker network ls -f name=host --format "{{ .ID }}"
-#if ($netId.Length -lt 1) {
-#    docker network create -d nat host
-#}
-
-#$cmd = "C:\k\kubelet.exe $global:KubeletArgs --cert-dir=$env:SYSTEMDRIVE\var\lib\kubelet\pki --config=/var/lib/kubelet/config.yaml --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --hostname-override=$(hostname) --pod-infra-container-image=`"mcr.microsoft.com/k8s/core/pause:1.2.0`" --enable-debugging-handlers --cgroups-per-qos=false --enforce-node-allocatable=`"`" --network-plugin=cni --resolv-conf=`"`" --log-dir=/var/log/kubelet --logtostderr=false --image-pull-progress-deadline=20m"
-
-#Invoke-Expression $cmd
-#}
+# https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubeadm
