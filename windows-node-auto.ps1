5ba81349b616c252ed07f7b315a3ed2db156c7d8
Leandro Scardua <leandroscar@gmail.com>
2022-03-15 06:09:18 -0700

    Update windows-node-auto.ps1

--- windows-node-auto.ps1
+++ windows-node-auto.ps1
@@ -25,7 +25,7 @@ Install-WindowsFeature -Name Containers | Out-Null
 
 # Install the Docker Engine Enterprise on the Server
 Write-Host "Installing Docker Engine"
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force | Out-Null
+Install-Module -Name DockerMsftProvider -Force | Out-Null
 
 # Install the client interface to manage the Docker Engine Enterprise
 Write-Host "Installing Docker Client"
