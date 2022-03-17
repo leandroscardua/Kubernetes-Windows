45b4312849f327b9750ab3a29d5f3c566ffab29d
Leandro Scardua <leandroscar@gmail.com>
2022-03-17 07:54:13 -0700

    Update Install-k8s-windows.ps1

--- Install-k8s-windows.ps1
+++ Install-k8s-windows.ps1
@@ -7,8 +7,8 @@
 
 # Installing requirements on the Windows Node to join Kubernetes Cluster
 
-curl.exe -LO https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
+curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
 #.\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
-powershell -NoLogo -Command "invoke-expression -Command c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
+powershell -NoLogo -Command "invoke-expression -Command c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion v1.23.4 -ContainerRuntime Docker"
 
 # https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubeadm
