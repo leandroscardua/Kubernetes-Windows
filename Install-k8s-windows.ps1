9032cf0b3f3bbe9d14762610c411988be6af5c6a
Leandro Scardua <leandroscar@gmail.com>
2022-03-17 08:18:24 -0700

    Update Install-k8s-windows.ps1

--- Install-k8s-windows.ps1
+++ Install-k8s-windows.ps1
@@ -6,9 +6,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Installing requirements on the Windows Node to join Kubernetes Cluster
-
 curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
 #.\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
-powershell -NoLogo -Command "invoke-expression -Command c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion v1.23.4 -ContainerRuntime Docker"
-
+c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
 # https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubeadm
