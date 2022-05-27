0e59fdbe3c331029237d13c30b02c67bf28525d9
Leandro Scardua <leandroscar@gmail.com>
2022-05-27 11:27:18 -0400

    Create Install-containerd-windows.ps1

---
+++ Install-containerd-windows.ps1
@@ -0,0 +1,14 @@
+#param(
+#  [Parameter(Mandatory=$true)]
+#  [String]$k8sversion
+#)
+# Enable Download
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Installing requirements on the Windows Node to join Kubernetes Cluster
+Write-Host "Downloadling ps1"
+curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
+#.\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
+Write-Host "Installing K8s"
+c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.24.0" -ContainerRuntime "ContainerD"
+# https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubeadm
