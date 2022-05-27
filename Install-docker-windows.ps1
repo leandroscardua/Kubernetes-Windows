caf22f35e18c6723bdbb18bf9073e6c036d94fef
Leandro Scardua <leandroscar@gmail.com>
2022-05-27 11:26:36 -0400

    Update and rename Install-k8s-windows.ps1 to Install-docker-windows.ps1

--- Install-docker-windows.ps1
+++ Install-docker-windows.ps1
@@ -10,5 +10,5 @@ Write-Host "Downloadling ps1"
 curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
 #.\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
 Write-Host "Installing K8s"
-c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
+c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.24.0" -ContainerRuntime "Docker"
 # https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubeadm
