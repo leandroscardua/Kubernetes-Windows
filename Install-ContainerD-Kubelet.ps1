898dcf77695e74e19dac3affcffa801beb9b3cf0
Leandro Scardua <leandroscar@gmail.com>
2022-06-02 15:12:29 -0400

    Update Install-ContainerD-Kubelet.ps1

--- Install-ContainerD-Kubelet.ps1
+++ Install-ContainerD-Kubelet.ps1
@@ -1,20 +1,21 @@
-#param(
-#  [Parameter(Mandatory=$true)]
-#  [String]$k8sversion
-#)
 # Enable Download
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$tagcd = (Invoke-WebRequest -UseBasicParsing "https://api.github.com/repos/containerd/containerd/releases/latest" | ConvertFrom-Json)[0].tag_name
+$lvcd = $tagcd -replace "v",""
+
+$tagk8s = (Invoke-WebRequest -UseBasicParsing "https://api.github.com/repos/kubernetes/kubernetes/releases/latest" | ConvertFrom-Json)[0].tag_name
+$lvk8s = $tagk8s -replace "v",""
 
 #Installing ContainerD
 curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/Install-Containerd.ps1 > c:\Windows\temp\Install-Containerd.ps1
 
-Write-Host "Installing ContainerD"
-c:\Windows\Temp\PrepareNode.ps1 -ContainerDVersion 1.6.4
+Write-Host "Installing ContainerD version: $tagcd"
+c:\Windows\Temp\Install-Containerd.ps1 -ContainerDVersion "$lvcd"
 # https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#joining-a-windows-worker-node
 
 # Installing requirements on the Windows Node to join Kubernetes Cluster
 curl.exe -L https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/kubeadm/scripts/PrepareNode.ps1 > c:\Windows\temp\PrepareNode.ps1
 #.\PrepareNode.ps1 -KubernetesVersion "v1.23.4" -ContainerRuntime "Docker"
-Write-Host "Installing Kubelet"
-c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "v1.24.0" -ContainerRuntime "ContainerD"
+Write-Host "Installing kubernetes version: $lvk8s"
+c:\Windows\Temp\PrepareNode.ps1 -KubernetesVersion "$tagk8s" -ContainerRuntime "ContainerD"
 # https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/adding-windows-nodes/#install-wins-kubelet-and-kubead
